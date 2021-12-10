ddbe392e8fe29a5c249e4ff721ba982fffbca038
Andrey Yamaev <a.yamaev@smartengines.com>
2021-12-10 17:56:36 +0300

    beutiful select class window

--- scenes/SelectClass/SelectClass.gd
+++ scenes/SelectClass/SelectClass.gd
@@ -4,12 +4,16 @@ extends Node
 # var a = 2
 # var b = "text"
 
+var player_base = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print('Hello world!')
 	pass # Replace with function body.
 
+func go_to_overview():
+	print("Overview")
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
