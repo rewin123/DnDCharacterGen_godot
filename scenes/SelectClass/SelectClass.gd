0af4dfdaef49e2bd6c3e8c2cbae00aa213d416d2
Andrey Yamaev <a.yamaev@smartengines.com>
2021-12-14 18:52:38 +0300

    панельки с персонажами

--- scenes/SelectClass/SelectClass.gd
+++ scenes/SelectClass/SelectClass.gd
@@ -5,11 +5,22 @@ extends Node
 # var b = "text"
 var class_overview_scene = load("res://scenes/class_overview.tscn")
 
+signal test_signal
+
 var player_base = {}
+
+func _on_dialogic_event(val):
+	print("Class ", val)
+	var overview = class_overview_scene.instance()
+	overview.common_setup(val)
+	add_child(overview)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print('Hello world!')
+	var new_dialog = Dialogic.start('SelectClass') 
+	add_child(new_dialog)
+	new_dialog.connect("dialogic_signal", self, "_on_dialogic_event")
 	pass # Replace with function body.
 
 func go_to_overview():
